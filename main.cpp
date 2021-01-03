9ac41e857c76b286d725a2aaf15b2be304503216
ghddbcks <54304944+ghddbcks@users.noreply.github.com>
2021-01-03 17:48:53 +0900

    반 완성

--- main.cpp
+++ main.cpp
@@ -1,198 +1,93 @@
 #include <SFML/Graphics.hpp>
-#include <deque>
-#include <utility>
-#include <iterator>
 #include <iostream>
-#include <vector>
+#include <deque>
 #include <cstdlib>
 #include <Windows.h>
+#include <algorithm>
+#include "texture.hpp"
 
-#define SNAKE 1
-#define APPLE 2
-#define EMPTY 0
-#define ILLEGAL 3
+#define SWIDTH 800
+#define SHEIGHT 900
 
-#define BWIDTH 100
-#define BHEIGHT 100
-#define BMARGIN 2 // px
+#define CWIDTH 25
+#define CHEIGHT 25
 
-#define SWIDTH 1000
-#define SHEIGHT 1000
+#define PWIDTH 25
+#define PHEIGHT 25
 
-#define DIED 2
-#define GROWN 1
-#define NOTHING 0
-
-#define SPEED .25f // second per move
-
-typedef int Square;
+#define POOPCNT 5
+#define PREGENSPD 1
 
 using namespace std;
 using namespace sf;
 
-class Board
+class Poop : public Sprite
 {
-private:
-    Square board[BWIDTH][BHEIGHT];
-
 public:
-    Board() : board{ 0 } {};
-
-    bool isLegal(int X, int Y)
+    Poop(const Texture& texture)
     {
-        return 0 <= X && X < BWIDTH && 0 <= Y && Y < BHEIGHT;
-    }
+        this->setTexture(texture);
+        this->setTextureRect(IntRect(25, 0, 25, 25));
 
-    Square getSquare(int X, int Y)
-    {
-        return board[X][Y];
-    }
-
-    void setSquare(int X, int Y, Square value)
-    {
-        board[X][Y] = value;
+        this->setPosition(rand() % SWIDTH, 0);
     }
 };
-
-class Snake
-{
-private:
-    deque<pair<int, int>> body; // first element is head, last element is tail
-    int dirX;
-    int dirY;
-    Board &board;
-
-public:
-    Snake(int X, int Y, Board& board, int length = 5) : dirX(1), dirY(0), board(board)
-    {
-        for (int i = 0;i < length;i++)
-        {
-            board.setSquare(X + dirX * i, Y + dirY * i, SNAKE);
-            body.push_front(pair<int, int>(X + dirX * i, Y + dirY * i));
-        }
-    }
-    
-    Snake(Board& board) : dirX(1), dirY(0), body(deque<pair<int, int>>()), board(board) {};
-
-    Snake(const Snake& lhs) : dirX(lhs.dirX), dirY(lhs.dirY), board(lhs.board), body(lhs.body) {};
-
-    Snake& operator=(const Snake& lhs)
-    {
-        dirX = lhs.dirX;
-        dirY = lhs.dirY;
-        board = lhs.board;
-        body = lhs.body;
-        return *this;
-    }
-
-    auto getBody()
-    {
-        return begin(body);
-    }
-
-    auto getHead()
-    {
-        return body.front();
-    }
-
-    int move()
-    {
-        auto head = body.front();
-        auto tail = body.back();
-        int goalX = head.first + dirX;
-        int goalY = head.second + dirY;
-        bool grow = false;
-
-        if (not board.isLegal(goalX, goalY))
-        {
-            return DIED;
-        }
-
-        Square element = board.getSquare(goalX, goalY);
-        if (element == SNAKE)
-        {
-            return DIED;
-        }
-
-        grow = element == APPLE;
-
-        body.push_front(pair<int, int>(goalX, goalY));
-        board.setSquare(goalX, goalY, SNAKE);
-
-        if (not grow)
-        {
-            body.pop_back();
-            board.setSquare(tail.first, tail.second, EMPTY);
-            return NOTHING;
-        }
-        return GROWN;
-    }
-
-    void setXdir(int value)
-    {
-        dirX = value;
-        dirY = 0;
-    }
-
-    void setYdir(int value)
-    {
-        dirY = value;
-        dirX = 0;
-    }
-};
-
-void newApple(Board& board)
-{
-    int i = 0;
-    int j = 0;
-
-    do
-    {
-        i = rand() % BWIDTH;
-        j = rand() % BHEIGHT;
-    } while (board.getSquare(i, j) != EMPTY);
-
-    board.setSquare(i, j, APPLE);
-}
-
-void reset(Board& board, Snake& snake)
-{
-    board = Board();
-    snake = Snake(5, BHEIGHT / 2 + 1, board);
-    newApple(board);
-}
 
 int main()
 {
     unsigned int t = GetTickCount64();
     srand(t);
 
-    RenderWindow window(VideoMode(SWIDTH, SHEIGHT), L"크고♂아름다운♂게이ㅁ");
+    RenderWindow window(VideoMode(SWIDTH, SHEIGHT), L"똥피하기");
 
     Event event;
 
-    Board board;
-    Snake snake(board);
-    reset(board, snake);
-
     Clock clock;
+    Clock pClock;
 
-    int result = 0;
+    int health;
 
-    vector<vector<RectangleShape>> squares = vector<vector<RectangleShape>>(BWIDTH, vector<RectangleShape>(BHEIGHT, RectangleShape()));
-    for (int i = 0;i < BWIDTH;i++)
+    int charSpeed;
+    int poopSpeed;
+
+    int movement;
+
+    deque<Poop> poops;
+    Sprite charactor;
+
+    Texture texture;
+    if (!texture.loadFromMemory(textures_png, textures_png_len))
     {
-        for (int j = 0;j < BHEIGHT;j++)
-        {
-            squares[i][j] = RectangleShape(Vector2f(SWIDTH / BWIDTH, SHEIGHT / BHEIGHT));
-            squares[i][j].setFillColor(Color::Black);
-            squares[i][j].setOutlineColor(Color::White);
-            squares[i][j].setOutlineThickness(BMARGIN / 2);
-            squares[i][j].setPosition(SWIDTH / BWIDTH * i, SHEIGHT / BHEIGHT * j);
-        }
+        cout << "Error!" << endl;
     }
 
-    Color colorTheme[3] = { Color::Black, Color::Green, Color::Red }; // empty, snake, apple
+    float dt;
+
+    auto reset = [&]()
+    {
+        clock.restart();
+        pClock.restart();
+
+        health = 3;
+
+        charSpeed = 300;
+        poopSpeed = 200;
+
+        movement = 0;
+
+        poops.push_back(Poop(texture));
+
+        charactor = Sprite(texture);
+        charactor.setTextureRect(IntRect(0, 0, 25, 25));
+        charactor.setPosition(SWIDTH / 2, SHEIGHT - CHEIGHT);
+    };
+
+    auto newPoop = [&]()
+    {
+        poops.push_back(Poop(texture));
+    };
+
+    reset();
 
     while (window.isOpen()) 
     {
@@ -207,50 +102,62 @@ int main()
             case Event::KeyPressed:
                 switch (event.key.code)
                 {
-                case Keyboard::W:
-                    snake.setYdir(-1);
+                case Keyboard::Left:
+                    movement = -1;
                     break;
-                case Keyboard::S:
-                    snake.setYdir(1);
-                    break;
-                case Keyboard::A:
-                    snake.setXdir(-1);
-                    break;
-                case Keyboard::D:
-                    snake.setXdir(1);
+                case Keyboard::Right:
+                    movement = 1;
                     break;
                 }
                 break;
-
             case Event::KeyReleased:
+                switch (event.key.code)
+                {
+                case Keyboard::Left:
+                    if (movement == -1) movement = 0;
+                    break;
+                case Keyboard::Right:
+                    if (movement == 1) movement = 0;
+                    break;
+                }
                 break;
             }
         }
 
-        if (clock.getElapsedTime().asSeconds() > SPEED)
+        dt = clock.getElapsedTime().asSeconds();
+        clock.restart();
+
+        for (auto& elem : poops)
         {
-            clock.restart();
-            result = snake.move();
+            elem.move(0, poopSpeed * dt);
 
-            switch (result)
+            if (elem.getGlobalBounds().intersects(charactor.getGlobalBounds()))
             {
-            case DIED:
-                reset(board, snake);
-                break;
-
-            case GROWN:
-                newApple(board);
+                cout << "dead" << endl;
             }
         }
-
-        for (int i = 0;i < BWIDTH;i++)
+        if (poops[0].getPosition().y > SHEIGHT + PHEIGHT)
         {
-            for (int j = 0;j < BHEIGHT;j++)
-            {
-                squares[i][j].setFillColor(colorTheme[board.getSquare(i, j)]);
-                window.draw(squares[i][j]);
-            }
+            poops.pop_front();
         }
+        charactor.move(charSpeed * movement * dt, 0);
+        charactor.setPosition(min(max(charactor.getPosition().x, 0.f), (float)(SWIDTH - CWIDTH)), SHEIGHT - CHEIGHT);
+
+        if (pClock.getElapsedTime().asSeconds() > PREGENSPD && poops.size() < POOPCNT)
+        {
+            poops.push_back(Poop(texture));
+            pClock.restart();
+        }
+
+        window.clear(Color::White);
+
+        window.draw(charactor);
+
+        for (auto elem : poops)
+        {
+            window.draw(elem);
+        }
+
         window.display();
     }
     return 0;
